1a5611bcfade669b1f4d3d908985dd1bc8526d38
Stefan Isaia <thenyartist@gmail.com>
2019-09-09 21:20:39 -0500

    Switching between components is now a thing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Nav extends React.Component {
     render() {
+        const { toggleNote, showNote } = this.props;
+        
         return (
             <div className='nav-container'>
                 <div className='nav-logo'>Icky Stickies</div>
-                <div className='nav-button'>
-                    + Note
+                <div className='nav-button' onClick={() => toggleNote()}>
+                    { showNote ? 'x Cancel' : '+ Note' }
                 </div>
             </div>
         )
